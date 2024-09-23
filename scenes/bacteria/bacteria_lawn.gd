c9e98ce6f515c52124d8dbfbac006487dc61cf1a
JerryShen <jerry82422@gmail.com>
2024-09-23 14:06:16 +0800

    Add growth curve to bacteria_lawn

--- scenes/bacteria/bacteria_lawn.gd
+++ scenes/bacteria/bacteria_lawn.gd
@@ -5,6 +5,9 @@ extends Node2D
 var center: Vector2
 var radius: float
 
+var elapsed_time: float
+var growth_phase: int
+
 func _ready() -> void:
 	center = global_position
 
@@ -16,20 +19,36 @@ func _ready() -> void:
 	$Timer.connect('timeout', spawn_bacteria)
 	spawn_bacteria()
 
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	if (elapsed_time < 10):
+		growth_phase = 0
+	elif (elapsed_time < 30):
+		growth_phase = 1
+	elif (elapsed_time < 60):
+		growth_phase = 2
+	else:
+		growth_phase = 3
 
 func spawn_bacteria() -> void:
-	var bacteria = bacteria_scene.instantiate()
-	bacteria.add_to_group("Bacteria")
+	var bacteria_count = 0
+	match growth_phase:
+		0: bacteria_count = 1 # Lag phase
+		1: bacteria_count = int(5 * elapsed_time / 20) # Logarithmic phase
+		2: bacteria_count = 10 # Stationary phase
+		3: bacteria_count = max(0, 10 - int((elapsed_time - 60) / 5)) # Decline phase
 
-	var random_angle = randf() * 2 * PI
-	var random_radius = sqrt(randf()) * radius
-	
-	var random_position = center + Vector2(
-		cos(random_angle) * random_radius,
-		sin(random_angle) * random_radius
-	)
-	
-	bacteria.global_position = random_position
+	for i in range(bacteria_count):
+		var bacteria = bacteria_scene.instantiate()
+		bacteria.add_to_group("Bacteria")
 
-
-	get_parent().add_child(bacteria)
+		var random_angle = randf() * 2 * PI
+		var random_radius = sqrt(randf()) * radius
+		
+		var random_position = center + Vector2(
+			cos(random_angle) * random_radius,
+			sin(random_angle) * random_radius
+		)
+		
+		bacteria.global_position = random_position
+		get_parent().add_child(bacteria)
